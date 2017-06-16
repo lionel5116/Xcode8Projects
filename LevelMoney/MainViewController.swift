82ca0d946e5cb6de780b47377ea1be85acbba1aa
lionel.jones <lioneljones5116@gmail.com>
2017-06-15 19:19:40 -0500

    Added Pods to include with main project

--- LevelMoney/MainViewController.swift
+++ LevelMoney/MainViewController.swift
@@ -47,7 +47,6 @@ class MainViewController: UIViewController,UITextFieldDelegate {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func showLoginFields(_ sender: Any) {
